a7c32d282a3d2661f03f633f959c3c16e7db6dd5
macwinnie <dev@macwinnie.me>
2022-03-01 21:42:37 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+ARG REG=""
+ARG REP="devopsansiblede"
+ARG IMG="apache"
+ARG VRS="php8"
+ARG SRC="${REG}${REP}/${IMG}:${VRS}"
+
+FROM "${SRC}"
+
+MAINTAINER macwinnie <dev@macwinnie.me>
+
+# copy all relevant files
+COPY files/ /
+COPY app/ "${APACHE_WORKDIR}"
+
+# organise file permissions and run installer
+RUN chmod a+x /install.sh && \
+    /install.sh && \
+    rm -f /install.sh
+
+# run on every (re)start of container
+ENTRYPOINT ["entrypoint"]
+CMD ["apache2-foreground"]
---
+++ files/install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -ex
+
+###
+## install missing tools
+###
+
+apt-get update -q --fix-missing
+apt-get -yq upgrade
+
+apt-get -yq install -y --no-install-recommends \
+    zlib1g-dev \
+    libicu-dev \
+    g++ \
+    libxml2-dev
+
+###
+## php extensions
+###
+docker-php-ext-install -j$(nproc) \
+    intl \
+    soap \
+    opcache
+
+###
+## CleanUp
+###
+
+set +e
+
+apt-get -y clean
+apt-get -y autoclean
+apt-get -y autoremove
+rm -r /var/lib/apt/lists/*
